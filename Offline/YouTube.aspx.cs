1bcc01d80451243fd38d4bf2b19fdaa839f19b98
Mygod <mygodstudio@gmail.com>
2014-04-06 09:37:30 +0800

    Improve YouTube offline downloading page 2

--- Offline/YouTube.aspx.cs
+++ Offline/YouTube.aspx.cs
@@ -11,14 +11,15 @@ namespace Mygod.Skylark.Offline
             if (string.IsNullOrWhiteSpace(url)) return;
             foreach (var video in Net.YouTube.Video.GetVideoFromLink(url))
             {
-                Response.Write(string.Format("<details><summary title=\"{3}\"><a href=\"{1}\">{0}</a>{2}</summary>",
-                    video.Title, video.Url, video.Url.Equals(url, StringComparison.InvariantCultureIgnoreCase)
-                        ? string.Empty : string.Format(" <a href=\"?Url={0}\" target=\"_blank\">[查看相关视频]</a>",
-                                                       Rbase64.Encode(video.Url)),
+                var isTarget = video.Url.Equals(url, StringComparison.InvariantCultureIgnoreCase);
+                Response.Write(string.Format("<details{4}><summary title=\"{3}\"><a href=\"{1}\">{0}</a>{2}</summary>",
+                    video.Title, video.Url, isTarget ? string.Empty : string.Format
+                        (" <a href=\"?Url={0}\" target=\"_blank\">[查看相关视频]</a>", Rbase64.Encode(video.Url)),
                     string.Format("标题：{0}{8}上传者：{1}{8}关键字：{2}{8}平均评分：{3}{8}观看次数：{4}{8}" +
                                   "上传时间：{5}{8}时长：{6}{8}地址：{7}", video.Title, video.Author,
                                   string.Join(", ", video.Keywords), video.AverageRating, video.ViewCount,
-                                  video.UploadTime.ToChineseString(), video.Length, video.Url, "&#10;")));
+                                  video.UploadTime.ToChineseString(), video.Length, video.Url, "&#10;"),
+                    isTarget ? " open" : string.Empty));
                 foreach (var link in video.FmtStreamMap)
                 {
                     Response.Write(string.Format(
