40e9767e63731acd2373cca494faa9bd4c80a6a7
Mygod <mygodstudio@gmail.com>
2014-06-07 14:44:28 +0800

    Test (2.0.11.255)

--- Offline/Start.aspx.cs
+++ Offline/Start.aspx.cs
@@ -15,6 +15,8 @@ namespace Mygod.Skylark.Offline
             var url = Rbase64.Decode(Request.QueryString["Url"].UrlDecode());
             var relativePath = RouteData.GetRelativePath();
             if (!string.IsNullOrWhiteSpace(url)) OfflineDownloadTask.Create(url, relativePath);
+            Response.Write(relativePath);
+            return;
             if ("True".Equals(Request.QueryString["Redirect"], StringComparison.InvariantCultureIgnoreCase))
                 Response.Redirect("/Browse/" + relativePath + '/');
             else Response.Write("<script>window.opener=null;window.close();</script>");
